168987e3fd5b632c1cdb05ce5e813d6c2a000709
William <WilliamLiang918@gmail.com>
2016-01-03 02:54:56 -0500

    Graphs selected columns.

--- index.js
+++ index.js
@@ -6,21 +6,77 @@ function upload(evt) {
   var reader = new FileReader();
   reader.readAsText(file);
   reader.onload = function(event) {
-     var csvData = event.target.result;
-     data = $.csv.toArrays(csvData);
-     if (data && data.length > 0) {
-       alert('Imported -' + data.length + '- rows successfully!');
-     } else {
-       alert('No data to import!');
-     }
+    var csvData = event.target.result;
+    data = $.csv.toArrays(csvData);
+    // if (data && data.length > 0) {
+    //  alert('Imported -' + data.length + '- rows successfully!');
+    // } else {
+    //  alert('No data to import!');
+    // }
+
+    data[0].forEach( function(label) {
+
+      for (var i = 0; i < 4; i++) {
+        var option = document.createElement("option");
+        option.text = label;
+        option.value = label;
+        $(".col")[i].appendChild(option);
+      }
+    });
   };
-}
 
-$("#graphcsv").on("click", function(){
+  $("#graphcsv").on("click", function(){
 
-  var file = $("#csvfile")[0].files[0];
+    var outerWidth = 500;
+    var outerHeight = 500;
+    var innerWidth = outerWidth - 30 - 30;
+    var innerHeight = outerHeight - 30 - 30;
+    var rMin = 1;
+    var rMax = 5;
+    var parsedData = [];
 
-  d3.csv(file.name, function (data) {
-    console.log("Inital Data", data);
+    var svg = d3.select("body").append("svg").
+      attr("width", outerWidth)
+      .attr("height", outerHeight);
+
+    var g = svg.append("g")
+      .attr("transform", "translate(30, 30)");
+
+    var xScale = d3.scale.linear().range([0, innerWidth]);
+    var yScale = d3.scale.linear().range([innerHeight, 0]);
+    var rScale = d3.scale.sqrt().range([rMin, rMax]);
+
+    for (var i = 1; i < data.length; i++){
+      parsedData.push({
+        name: data[i][data[0].indexOf($(".name")[0].value)],
+        xColumn: data[i][data[0].indexOf($(".xColumn")[0].value)],
+        yColumn: data[i][data[0].indexOf($(".yColumn")[0].value)],
+        rColumn: data[i][data[0].indexOf($(".rColumn")[0].value)],
+      });
+    }
+
+    function render(data) {
+      xScale.domain(d3.extent(data, function(d) { return +d.xColumn; }));
+
+      yScale.domain(d3.extent(data, function(d) { return +d.yColumn; }));
+
+      var circles = g.selectAll("circle").data(data);
+      circles.enter().append("circle");
+
+      circles
+        .attr("cx", function (d) { return xScale(+d.xColumn); })
+        .attr("cy", function (d) { return yScale(+d.yColumn); })
+        .attr("r", 5)
+        .attr("class", function(d) { return d.name; });
+
+      circles.exit().remove();
+
+      debugger
+
+    }
+
+    render(parsedData);
   });
-});
+
+
+}
