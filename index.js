da45de6e264c9e329ba9be22746df8e7a1681e6d
William <WilliamLiang918@gmail.com>
2016-01-04 15:59:01 -0500

    Fix axes label.

--- index.js
+++ index.js
@@ -34,31 +34,41 @@ function upload(evt) {
     var xAxisLabelOffset = 48;
     var yAxisLabelOffset = 48;
     var titleLabelOffset = 10;
-    var margin = {left: 60, top: (20 + titleLabelOffset), right: 5, bottom: 60 };
+    var margin = {left: 60, top: (20 + titleLabelOffset), right: 5, bottom: (60) };
     var innerWidth = outerWidth - margin.left - margin.right;
     var innerHeight = outerHeight - margin.top - margin.bottom;
+    console.log(innerHeight);
     var rMin = 1;
     var rMax = 5;
     var parsedData = [];
-    var xAxisLabelText = $(".xColumn")[0].value + " (" +
-                         $('input[name=xScale]:checked').val() + ")";
-    var yAxisLabelText = $(".yColumn")[0].value + " (" +
-                         $('input[name=yScale]:checked').val() + ")";
-    var titleLabelText = $(".yColumn")[0].value + " vs " + $(".xColumn")[0].value;
+    var xColumnName = $(".xColumnName")[0].value;
+    var xColumn = $(".xColumn")[0].value;
+    var rColumn = $(".rColumn")[0].value;
+    var yColumn = $(".yColumn")[0].value;
+    var yColumnName = $(".yColumnName")[0].value;
+    var labelArugmentName = $(".label-arugment-name")[0].value;
+    var labelArugment = $(".label-arugment")[0].value;
 
-    var svg = d3.select("body").append("svg").
-      attr("width", outerWidth)
+    var xAxisLabelText = (xColumnName === "") ? xColumn : xColumnName;
+    var yAxisLabelText = (yColumnName === "") ? yColumn : yColumnName;
+    var labelArugmentText = (labelArugmentName === "") ?
+                          (labelArugment == "default" ? "Index" : labelArugment) :
+                          labelArugmentName;
+    var titleLabelText = yAxisLabelText + " vs " + xAxisLabelText;
+
+    var svg = d3.select("body").append("svg")
+      .attr("width", outerWidth)
       .attr("height", outerHeight);
 
     var titleG = svg.append("g")
-      .attr("transform", "translate(0," + titleLabelOffset + ")")
+      .attr("transform", "translate(" + (margin.left / 2) + "," + titleLabelOffset + ")")
       .attr("class", "label");
 
     var titleLabel = titleG.append("text")
       .style("text-anchor", "middle")
       .attr("x", outerWidth / 2)
       .attr("y", titleLabelOffset)
-      .attr("class", "label title")
+      .attr("class", "label titleLabel")
       .text(titleLabelText);
 
     var g = svg.append("g")
@@ -66,7 +76,7 @@ function upload(evt) {
 
     var xAxisG = g.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + innerHeight + ")");
+      .attr("transform", "translate(0," + (innerHeight) + ")");
     var yAxisG = g.append("g")
       .attr("class", "y axis");
 
@@ -75,48 +85,50 @@ function upload(evt) {
       .attr("x", innerWidth / 2)
       .attr("y", xAxisLabelOffset)
       .attr("class", "label")
-      .text(xAxisLabelText);
+      .text(xAxisLabelText + $('input[name=xScale]:checked').val());
+
     var yAxisLabel = yAxisG.append("text")
       .style("text-anchor", "middle")
       .attr("transform", "translate(-" + yAxisLabelOffset + "," + (innerHeight / 2) + ") rotate(-90)")
       .attr("class", "label")
-      .text(yAxisLabelText);
+      .text(yAxisLabelText + $('input[name=yScale]:checked').val());
 
-    var xScale = $('input[name=xScale]:checked').val() == "linear" ?
+    var xScale = $('input[name=xScale]:checked').val() === "" ?
                   d3.scale.linear().range([0, innerWidth]) :
                   d3.scale.log().range([0, innerWidth]);
 
-    var yScale = $('input[name=yScale]:checked').val() == "linear" ?
-                  d3.scale.linear().range([(innerWidth - xAxisLabelOffset), 0]) :
-                  d3.scale.log().range([(innerWidth - xAxisLabelOffset), 0]);
+    var yScale = $('input[name=yScale]:checked').val() === "" ?
+                  d3.scale.linear().range([(innerHeight), 0]) :
+                  d3.scale.log().range([(innerHeight), 0]);
 
     var rScale = d3.scale.sqrt().range([rMin, rMax]);
 
     for (var i = 1; i < data.length; i++){
       parsedData.push({
-        name: data[i][data[0].indexOf($(".name")[0].value)],
-        xColumn: data[i][data[0].indexOf($(".xColumn")[0].value)],
-        yColumn: data[i][data[0].indexOf($(".yColumn")[0].value)],
-        rColumn: data[i][data[0].indexOf($(".rColumn")[0].value)],
+        label: (labelArugment == "default") ? i :
+                data[i][data[0].indexOf(labelArugment)],
+        xColumn: data[i][data[0].indexOf(xColumn)],
+        yColumn: data[i][data[0].indexOf(yColumn)],
+        rColumn: data[i][data[0].indexOf(rColumn)],
       });
     }
 
     var xAxis = d3.svg.axis().scale(xScale).orient("bottom")
-      .ticks(3)
+      .ticks(5)
       .tickFormat(d3.format("s"))
-      .outerTickSize(0);
+      .outerTickSize(1);
     var yAxis = d3.svg.axis().scale(yScale).orient("left")
-      .ticks(3)
+      .ticks(5)
       .tickFormat(d3.format("s"))
-      .outerTickSize(0);
+      .outerTickSize(1);
 
     function render(data) {
       xScale.domain(d3.extent(data, function(d) { return +d.xColumn; }));
       yScale.domain(d3.extent(data, function(d) { return +d.yColumn; }));
       rScale.domain(d3.extent(data, function(d) { return +d.rColumn; }));
 
-      if ($('input[name=xScale]:checked').val() == "linear") { xAxisG.call(xAxis); }
-      if ($('input[name=yScale]:checked').val() == "linear") { yAxisG.call(yAxis); }
+      if ($('input[name=xScale]:checked').val() === "") { xAxisG.call(xAxis); }
+      if ($('input[name=yScale]:checked').val() === "") { yAxisG.call(yAxis); }
 
       var circles = g.selectAll("circle").data(data);
       circles.enter().append("circle");
@@ -136,9 +148,9 @@ function upload(evt) {
           tooltip.transition()
             .duration(200)
             .style("opacity", 0.9);
-          tooltip.html("Name: " + d.name + "<br/>" +
-                      $(".xColumn")[0].value + ": " + d.xColumn.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,") + "<br/>" +
-                      $(".yColumn")[0].value + ": " + d.yColumn.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,"))
+          tooltip.html(labelArugmentText + ": " + d.label + "<br/>" +
+                      xAxisLabelText + ": " + d.xColumn.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,") + "<br/>" +
+                      yAxisLabelText + ": " + d.yColumn.replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,"))
             .style("left", (d3.event.pageX) + "px")
             .style("top", (d3.event.pageY - 28) + "px");
         })
